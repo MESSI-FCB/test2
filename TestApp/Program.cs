5e3da728b7fbdebbee2c5824440b0f8d0d041904
WIN10-803251146\messi <MESSI-FCB>
2018-03-31 17:19:41 +0800

    delete

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,20 +1,16 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
 
-namespace StarApp
+namespace TestApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-          
-            //12332232323
-            //444
+            ///hahhahhahhhh
         }
     }
 }
